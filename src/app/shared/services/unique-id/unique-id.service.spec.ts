ad5db64bd877a5cb8b808d1ada1ea09666d1dca6
Study Profile <dee.alves2@gmail.com>
2021-06-14 00:01:10 -0300

    Validando métodos

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,11 +1,41 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+  let service: UniqueIdService = null;
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPredfix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
-    const id = service.generateUniqueIdWithPredfix('app');
-    expect(id).toContain('app-');
+  beforeEach(() => {
+    service = new UniqueIdService();
   });
 
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPredfix.name}
+    should generate id when called with prefix`, () => {
+    const id = service.generateUniqueIdWithPredfix('app');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPredfix.name}
+    should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPredfix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generated IDs when called`, () => {
+    service.generateUniqueIdWithPredfix('app');
+    service.generateUniqueIdWithPredfix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPredfix.name}
+    should throw when called with empty`, () => {
+    const emptyValues = [null, undefined, '', '0', '1'];
+    emptyValues.forEach(emptyValue => {
+      expect(() => service.generateUniqueIdWithPredfix(emptyValue))
+        .withContext(`Empty value: ${emptyValue}`)
+        .toThrow();
+    });
+  });
 });
