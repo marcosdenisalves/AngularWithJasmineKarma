b6a08f0e7cccdf2ec86c135f2fae8e9e47e52f30
Study Profile <dee.alves2@gmail.com>
2021-06-14 00:52:11 -0300

    Criação do componente like

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget/like-widget.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeWidgetComponent } from './shared/components/like-widget/like-widget/like-widget.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LikeWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
